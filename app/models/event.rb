a40676d60458d7ccf8e5669b1908ccfe50095df8
zmrok <weinbloodlust@gmail.com>
2022-09-26 20:11:46 +0300

    Separated past and upcoming events on event Index page via scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,7 @@ class Event < ApplicationRecord
 
   has_many :attendances, foreign_key: :event_id, class_name: 'Attend'
   has_many :attendees, through: :attendances
+
+  scope :past_events, -> { where('event_date <= ?', Time.zone.now).order(event_date: :desc) }
+  scope :upcoming_events, -> { where('event_date > ?', Time.zone.now).order(event_date: :desc) }
 end
