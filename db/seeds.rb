2b4873e35fd8c350406461239818653c7b0efe25
zmrok <weinbloodlust@gmail.com>
2022-10-26 21:07:37 +0300

    created seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if Rails.env.development?
+  Attend.delete_all
+  Event.delete_all
+  User.delete_all
+end
+
+#creating users
+(1..10).each do |i|
+  user = User.new
+  user.email = "user#{i}@gmail.com"
+  user.password = "pwd#{i}"
+  user.password_confirmation = "pwd#{i}"
+  user.save
+end
+
+# creating events
+(1..100).each do |i|
+  user = User.all.sample
+  event_type = [true, false].sample
+  flag = event_type ? 'OPEN' : 'PRIVATE'
+  title = "Event number #{i}. #{flag}"
+  description = "Some description, bla-bla-bla, #{i}"
+  location = 'Minsk'
+  event_date = (-100..100).to_a.sample.days.from_now
+  Event.create(user_id: user.id,
+               title: title,
+               description: description,
+               location: location,
+               event_date: event_date,
+               event_type: event_type)
+end
+
+#attending_events
+Event.all.each do |event|
+  5.times do
+    user = User.all.sample
+    Attend.create(attendee_id: user.id, event_id: event.id) unless user == event.creator
+  end
+end
